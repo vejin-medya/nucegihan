ccd8dc4bcda88afb603eb684da5e6beb6bbc8e41
cizira-botan <cezirabotan@gmail.com>
2025-01-08 05:13:32 +0300

    initial

--- wsgi.py
+++ wsgi.py
@@ -1,18 +1,8 @@
 import os
 from app.main import app
 from app.newsFetcher import NewsFetcher
-from app.database_manager import DatabaseManager
-from dotenv import load_dotenv
 from app.scheduler import Scheduler
 
-
-load_dotenv()
-db_url = os.getenv('DATABASE_URL')
-print(db_url)
-if not db_url:
-    raise ValueError("DATABASE_URL is not set in the environment variables.")
-
-db_manager = DatabaseManager(db_url)
 if __name__ == '__main__':
     NewsFetcher.fetch_and_save_allNews()
     # Zamanlayıcıyı başlat
