92c59dae2599be3f137279bd813a329b6bd04fb3
cizira-botan <cezirabotan@gmail.com>
2025-01-08 19:56:05 +0300

    cana min

--- app/clock.py
+++ app/clock.py
@@ -5,6 +5,6 @@ sched = BlockingScheduler()
 @sched.scheduled_job('interval', minutes=10)
 def timed_job():
     NewsFetcher.fetch_and_save_allNews()
-    print('This job is run every three minutes.')
+    print('This job is run every three minutes')
 
 sched.start()
